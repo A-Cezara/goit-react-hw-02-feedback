e008fa1fc96e60b02b98bac0113338e32d23fe14
A-Cezara <alexandracezara@yahoo.ro>
2024-07-27 16:27:18 +0300

    Feedback complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,62 @@
-import React from 'react';
-import Counter from './Counter/Counter'; 
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
+  handleIncrement = (type) => {
+    this.setState((prevState) => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Please leave feedback</h1>
-      <Counter step={1} />
-    </div>
-  );
+  calculatePositivePercentage() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total ? Math.round((good / total) * 100) : 0;
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = this.calculatePositivePercentage();
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleIncrement}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  step: PropTypes.number,
+};
 
 export default App;

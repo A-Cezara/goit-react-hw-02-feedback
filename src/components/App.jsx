ddbc7ea7b1eb78b95779f17f404891461bff4cc7
A-Cezara <alexandracezara@yahoo.ro>
2024-08-15 17:09:34 +0300

    updates task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
+
+
+export default function App() {
+
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  })
+  const { good, neutral, bad } = state;
+  const total = good + neutral + bad;
+  const positivePercentage = calculatePositivePercentage();
 
-  handleIncrement = (type) => {
-    this.setState((prevState) => ({
+
+  function handleIncrement (type) {
+    setState((prevState) => ({
+      ...prevState,
       [type]: prevState[type] + 1,
     }));
   };
 
-  calculatePositivePercentage() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+  function calculatePositivePercentage() {
     return total ? Math.round((good / total) * 100) : 0;
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = this.calculatePositivePercentage();
-
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleIncrement}
+  return (
+    <div className="app-container">
+      <Section title="Please leave a feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleIncrement}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback." />
+        )}
+      </Section>
+    </div>
+  );
 }
 
 App.propTypes = {
   step: PropTypes.number,
 };
-
-export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div>
-    {options.map((option) => (
-      <button
-        key={option}
-        type="button"
-        className={styles.button}
-        onClick={() => onLeaveFeedback(option)}
-      >
-        {option}
-      </button>
-    ))}
-  </div>
-);
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div className={styles.feedbackButtons}>
+      {options.map(option => (
+        <button
+          key={option}
+          className={styles.feedbackButton}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option.charAt(0).toUpperCase() + option.slice(1)}
+        </button>
+      ))}
+    </div>
+  );
+}
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
